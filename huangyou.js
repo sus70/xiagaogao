78c64a0e008fa458278b010dfb398d01e458eaa6
sus70 <97860985+sus70@users.noreply.github.com>
2022-01-17 16:35:37 +0800

    Update huangyou.js

--- huangyou.js
+++ huangyou.js
@@ -1,2 +1,3 @@
-body = $response.body.replace(/memberships:\\[\\]/g, 'memberships\":[{\"endAt\":1650842277,\"id\":\"1\",\"name\":\"普通会员\",\"ownership\":\"temporary\",\"startAt\":1580106429,\"usageType\":\"unlimited\"}]')..replace(/ownership\":\"\\w+/g, 'ownership\":\"free');
+body = $response.body.replace(/memberships:\[\]/g, 'memberships\":[{"endAt":1650842277,"id":"1","name":"普通会员","ownership":"temporary","startAt":1580106429,"usageType":"unlimited"}]')..replace(/usageType":"\w+/g, 'usageType":"unlimited"');
 $done({body});
+
